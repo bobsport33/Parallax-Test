704980a4f3bbd1bf8c42ea91e3aca6850edd634a
bob <bkelly@impactnetworking.com>
2022-12-19 12:21:13 -0600

    inital commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ bones = document.querySelectorAll(".bone");
 
 bones.forEach((bone) => {
     bone.style.top = `${Math.floor(Math.random() * 2000)}px`;
-    bone.style.left = `${Math.floor(Math.random() * 1000)}px`;
+    bone.style.left = `${Math.floor(Math.random() * 1600)}px`;
     let size = Math.floor(Math.random() * 300);
     bone.style.height = `${size}px`;
     bone.style.width = `${size}px`;
@@ -14,6 +14,6 @@ gsap.to(".bone", {
     scrollTrigger: {
         scrub: true,
     },
-
+    rotate: 360,
     y: () => -ScrollTrigger.maxScroll(window) * 0.2,
 });
